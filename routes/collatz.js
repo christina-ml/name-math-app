73617d0f3dd421410d6542e63f004e94aac06dc7
Christina Loiacono <loiaconochristina@gmail.com>
2023-06-06 13:45:58 -0400

    add memoization to collatz

--- routes/collatz.js
+++ routes/collatz.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 const collatz = require('../utils/collatz');
+/* 
+Memoization:
+  an object, save it to an object
+  as processing a number, check if that n is already in the object
+*/
+
+// variable for object to save collatz values of numbers already checked
+let memoObj = {};
 
 router.get("/:num", (req, res) => {
 	const { num } = req.params;
@@ -15,8 +23,13 @@ router.get("/:num", (req, res) => {
       throw "num must be 1 or greater"
     } 
 
-    const collatzValue = collatz(num); // recursive function
-		res.status(200).send(`the collatz value of ${num} is ${collatzValue}`);
+    // add collatz value to memoObject
+    if (!memoObj[num]){
+      console.log("num first time");
+      const collatzValue = collatz(num, '', memoObj); // recursive function
+      memoObj[num] = collatzValue;
+    }
+		res.status(200).send(`the collatz value of ${num} and memoObj is ${memoObj[num]}`);
 
 	} catch (error){
 		res.status(500).send(error);
--- utils/collatz.js
+++ utils/collatz.js
@@ -1,10 +1,23 @@
-function collatz(n, str = ''){
-  if(n === 1) return str + '1';
-  
-  str += n + '->';
-  
-  n = n % 2 === 0 ? n / 2: n*3 + 1;
-  
-  return collatz(n, str);
-}
+function collatz(n, str = '', memoObj){
+    console.log(memoObj)
+    // if the number exists in memoObj
+    if (memoObj[n]){
+        console.log(`${n} is found in memoObj`)
+        // return string + key at memoObj[number]
+        return str + memoObj[n];
+    }
+
+    if (n === 1) return str + 1;
+    
+    if (n % 2 === 0){
+      str += n + "->";
+      n = n/2;
+    } else {
+      str += n + "->";
+      n = (3 * n) + 1;
+    }
+    
+    // call repeatedly until n is equal to 1
+    return collatz(n, str, memoObj);
+  }
 module.exports = collatz;
